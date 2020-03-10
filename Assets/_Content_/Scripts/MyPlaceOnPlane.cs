d10d79ff3f76da70db90c97a181952b6a5a1600c
kevin.galleron <kevin.galleron@accenture.com>
2020-03-10 23:50:52 +0100

    initial commit

---
+++ Assets/_Content_/Scripts/MyPlaceOnPlane.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class MyPlaceOnPlane : PlaceOnPlane
+{
+    private bool _touchDetectionIsActive;
+    private MyPlaneDetectionController _myPlaneDetectionController;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        this._touchDetectionIsActive = true;
+        this._myPlaneDetectionController = GetComponent<MyPlaneDetectionController>();
+    }
+
+    protected override void Update()
+    {
+        if (!this._touchDetectionIsActive)
+            return;
+
+        base.Update();
+
+        if (spawnedObject != null)
+        {
+            ToggleTouchTrackingAndPlaneDetectionAndPointCloud();
+        }
+    }
+
+    public void ToggleTouchTrackingAndPlaneDetectionAndPointCloud()
+    {
+        this._touchDetectionIsActive = false;
+        this._myPlaneDetectionController.TogglePlaneDetectionAndPointCloud();
+    }
+}
---
+++ Assets/_Content_/Scripts/MyPlaneDetectionController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class MyPlaneDetectionController : PlaneDetectionController
+{
+    private ARPointCloudManager _aRPointCloudManager;
+
+    private void Start()
+    {
+        this._aRPointCloudManager = GetComponent<ARPointCloudManager>();
+    }
+
+    public void TogglePlaneDetectionAndPointCloud()
+    {
+        base.TogglePlaneDetection();
+
+        var points = this._aRPointCloudManager.trackables;
+
+        foreach (var pts in points)
+        {
+            pts.gameObject.SetActive(false);
+        }
+
+        this._aRPointCloudManager.enabled = false;
+    }
+}
