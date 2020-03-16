3f41204b3b6fef2903e4cb018adc77a66ee0b21b
alice.haupais <alice.haupais@accenture.com>
2020-03-16 19:33:28 +0100

    Clean archi-spawn basket ball net

---
+++ Assets/_Content_/Scripts/infrastructure/PlaneDetectionARRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneDetectionARRepository : PlaneDetection, MonoBehaviour
+
+{
+    private MyPlaneDetectionController _myPlaneDetectionController;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        this._myPlaneDetectionController = GetComponent<MyPlaneDetectionController>();
+    }
+
+    public void togglePlaneDetectionAndPointCloud()
+    {
+        _myPlaneDetectionController.TogglePlaneDetectionAndPointCloud();
+    }
+
+}
---
+++ Assets/_Content_/Scripts/use cases/SpawnBasketBallNet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class SpawnBasketBallNet : PlaceOnPlane
+{
+    private bool firstTouchDetection;
+    private PlaneDetectionRepository planeDetectionRepository;
+    private SwipeControllerRepository swipeControllerRepository;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        this.firstTouchDetection = true;
+        this.planeDetectionRepository = new PlaneDetectionARRepository();
+        this.swipeControllerRepository = new SwipeControllerARRepository();
+        this._swipeController = GetComponent<SwipeController>();
+    }
+
+    protected override void Update()
+    {
+        if (!this.firstTouchDetection)
+            return;
+
+        base.Update();
+
+        if (spawnedObject != null)
+        {
+            TogglePlaneDetectionAndPointCloud();
+            this.swipeControllerRepository.enableSwipeController();
+        }
+    }
+
+    public void TogglePlaneDetectionAndPointCloud()
+    {
+        this.firstTouchDetection = false;
+        this.planeDetectionRepository.togglePlaneDetectionAndPointCloud();
+    }
+}
